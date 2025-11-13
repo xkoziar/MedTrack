4fea4ca80fcc884531676df48aab7f62ba243b5e
Ivan <567817@muni.cz>
2025-11-13 23:26:18 +0100

    custom text field added

--- lib/main.dart
+++ lib/main.dart
@@ -10,5 +10,5 @@ Future<void> main() async {
 
   IocContainer.initialize();
 
-  runApp(const AppWrapper());
+  runApp(AppWrapper());
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,28 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:med_track/database/model/user.dart';
-import 'package:med_track/database/service/user_database_service.dart';
-
-import '../database/ioc/ioc_container.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
 
-  final UserDatabaseService _userService = get<UserDatabaseService>();
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Movies Rating')),
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            _userService.addUser(
-              User(id: '0', name: 'John Doe', email: 'email@google.com'),
-            );
-          },
-          child: const Text('Add User'),
-        ),
-      ),
-    );
+    return Placeholder();
   }
 }
