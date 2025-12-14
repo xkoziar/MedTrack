d919fcf149582121cb8b7a371ba15d4ec1895c2f
Ivan <567817@muni.cz>
2025-12-14 21:57:49 +0100

    mocks updated

--- lib/app_shell.dart
+++ lib/app_shell.dart
@@ -5,9 +5,6 @@ import 'package:med_track/pages/medication_page.dart';
 import 'package:med_track/pages/profile_page.dart';
 
 import 'package:med_track/pages/medication_details_page.dart';
-import 'package:med_track/utils/helpers/create_mock_data.dart';
-
-import 'database/model/dose_event.dart';
 import 'database/model/medication.dart';
 
 class AppShell extends StatefulWidget {
@@ -21,76 +18,12 @@ class _AppShellState extends State<AppShell> {
   int _index = 0;
 
   late final Medication _mockMedication;
-  late final List<DoseEvent> _mockEvents;
 
   @override
   void initState() {
     super.initState();
 
-    final now = DateTime.now();
-
     //createMockData(); <--- uprav user id
-    _mockMedication = Medication(
-      id: 'med_test_1',
-      userId: 'user_test_1',
-      name: 'Ibuprofen',
-      description: 'After meals. Don’t combine with alcohol.',
-      dosage: '200 mg',
-      startDate: now.subtract(const Duration(days: 10)),
-      endDate: null,
-      isActive: false,
-      scheduleDays: const [1, 2, 3, 4, 5, 6, 7],
-      scheduleTimes: const ['08:00', '20:00'],
-      createdAt: now.subtract(const Duration(days: 10)),
-      updatedAt: now,
-    );
-
-    _mockEvents = [
-      DoseEvent(
-        id: 'e1',
-        userId: 'user_test_1',
-        medicationId: 'med_test_1',
-        scheduledAt: DateTime(
-          now.year,
-          now.month,
-          now.day,
-        ).subtract(const Duration(days: 2, hours: -8)),
-        takenAt: DateTime(
-          now.year,
-          now.month,
-          now.day,
-        ).subtract(const Duration(days: 2, hours: -8, minutes: -6)),
-        status: DoseStatus.taken,
-      ),
-      DoseEvent(
-        id: 'e2',
-        userId: 'user_test_1',
-        medicationId: 'med_test_1',
-        scheduledAt: DateTime(
-          now.year,
-          now.month,
-          now.day,
-        ).subtract(const Duration(days: 1, hours: -20)),
-        takenAt: null,
-        status: DoseStatus.missed,
-      ),
-      DoseEvent(
-        id: 'e3',
-        userId: 'user_test_1',
-        medicationId: 'med_test_1',
-        scheduledAt: DateTime(now.year, now.month, now.day, 8, 0),
-        takenAt: DateTime(now.year, now.month, now.day, 8, 7),
-        status: DoseStatus.taken,
-      ),
-      DoseEvent(
-        id: 'e4',
-        userId: 'user_test_1',
-        medicationId: 'med_test_1',
-        scheduledAt: DateTime(now.year, now.month, now.day, 20, 0),
-        takenAt: null,
-        status: DoseStatus.pending,
-      ),
-    ];
   }
 
   List<Widget> get _pages => [
@@ -100,7 +33,6 @@ class _AppShellState extends State<AppShell> {
     const HomePage(),
     MedicationDetailPage(
       medication: _mockMedication,
-      recentEvents: _mockEvents,
       nfcTagId: '04:A2:7F:19:CC:2B:80',
       onPairNfc: () {
         ScaffoldMessenger.of(
